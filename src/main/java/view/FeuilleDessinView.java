36a468780565f6d468a34fb752f85293e7b2c586
Florian DE LA COMBLE <f.delacomble@gmail.com>
2017-05-17 08:48:53 +0200

    color ok

--- src/main/java/view/FeuilleDessinView.java
+++ src/main/java/view/FeuilleDessinView.java
@@ -64,8 +64,41 @@ public class FeuilleDessinView extends JPanel implements Observer {
     public void showTurtles(Graphics g) {
         for (Iterator it = tortueViews.iterator(); it.hasNext();) {
             TortueView tortueView = (TortueView) it.next();
+            g.setColor(getTortueColor(tortueView));
             Polygon polygon = tortueView.getDessinTortue();
             g.fillPolygon(polygon);
+        }
+    }
+
+    private Color getTortueColor(TortueView tortueView) {
+        int couleurNb = tortueView.getCouleur();
+        switch (couleurNb) {
+            case 0:
+                return (Color.black);
+            case 1:
+                return (Color.blue);
+            case 2:
+                return (Color.cyan);
+            case 3:
+                return (Color.darkGray);
+            case 4:
+                return (Color.red);
+            case 5:
+                return (Color.green);
+            case 6:
+                return (Color.lightGray);
+            case 7:
+                return (Color.magenta);
+            case 8:
+                return (Color.orange);
+            case 9:
+                return (Color.gray);
+            case 10:
+                return (Color.pink);
+            case 11:
+                return (Color.yellow);
+            default:
+                return (Color.black);
         }
     }
 
