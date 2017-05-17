2a1914c99d104a9b506c84e92126732afd053477
lucas.g <lucas.g@apps-panel.com>
2017-05-17 10:39:20 +0200

    new button in menu

--- src/main/java/view/SimpleLogoView.java
+++ src/main/java/view/SimpleLogoView.java
@@ -15,147 +15,148 @@ import java.awt.event.WindowEvent;
 
 /*************************************************************************
 
-	Un petit Logo minimal qui devra etre ameliore par la suite
+ Un petit Logo minimal qui devra etre ameliore par la suite
 
-				J. Ferber - 1999-2001
+ J. Ferber - 1999-2001
 
-				Cours de DESS TNI - Montpellier II
+ Cours de DESS TNI - Montpellier II
 
-	@version 2.0
-	@date 25/09/
+ @version 2.0
+ @date 25/09/
 
 
-**************************************************************************/
+ **************************************************************************/
 
 
 public class SimpleLogoView extends JFrame {
-	public static final Dimension VGAP = new Dimension(1,5);
-	public static final Dimension HGAP = new Dimension(5,1);
+    public static final Dimension VGAP = new Dimension(1, 5);
+    public static final Dimension HGAP = new Dimension(5, 1);
 
-	private FeuilleDessinView feuilleDessinView;
-	private JTextField inputValue;
-	private ControllerSimpleLogo controller;
-
-	
-	public void quitter() {
-		System.exit(0);
-	}
-
-	public SimpleLogoView(ControllerSimpleLogo controller) {
-		super("un logo tout simple");
-		this.feuilleDessinView = new FeuilleDessinView();
-		this.controller = controller;
-		logoInit();
-		
-		addWindowListener(new WindowAdapter() {
-		    @Override
-		    public void windowClosing(WindowEvent arg0) {
-		        super.windowClosing(arg0);
-		        System.exit(0);
-		    }
-		});
-	}
-
-	public void addTortue(Tortue tortue) {
-		feuilleDessinView.addTortue(tortue);
-	}
-
-	public void logoInit() {
-		getContentPane().setLayout(new BorderLayout(10,10));
-
-		// Boutons
-		JToolBar toolBar = new JToolBar();
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(toolBar);
-
-		getContentPane().add(buttonPanel,"North");
-
-		addButton(toolBar,"Effacer","Nouveau dessin","/icons/index.png");
-		
-		toolBar.add(Box.createRigidArea(HGAP));
-		inputValue=new JTextField("45",5);
-		toolBar.add(inputValue);
-		addButton(toolBar, "Avancer", "Avancer 50", null);
-		addButton(toolBar, "Droite", "Droite 45", null);
-		addButton(toolBar, "Gauche", "Gauche 45", null);
-		addButton(toolBar, "Lever", "Lever Crayon", null);
-		addButton(toolBar, "Baisser", "Baisser Crayon", null);
-
-		String[] colorStrings = {"noir", "bleu", "cyan","gris fonce","rouge",
-								 "vert", "gris clair", "magenta", "orange",
-								 "gris", "rose", "jaune"};
-
-		// Create the combo box
-		toolBar.add(Box.createRigidArea(HGAP));
-		JLabel colorLabel = new JLabel("   Couleur: ");
-		toolBar.add(colorLabel);
-		JComboBox colorList = new JComboBox(colorStrings);
-		toolBar.add(colorList);
-
-		colorList.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JComboBox cb = (JComboBox)e.getSource();
-				int n = cb.getSelectedIndex();
-			}
-		});
+    private FeuilleDessinView feuilleDessinView;
+    private JTextField inputValue;
+    private ControllerSimpleLogo controller;
 
 
-		// Menus
-		JMenuBar menubar=new JMenuBar();
-		setJMenuBar(menubar);	// on installe le menu bar
-		JMenu menuFile=new JMenu("File"); // on installe le premier menu
-		menubar.add(menuFile);
+    public void quitter() {
+        System.exit(0);
+    }
 
-		addMenuItem(menuFile, "Effacer", "Effacer", KeyEvent.VK_N);
-		addMenuItem(menuFile, "Quitter", "Quitter", KeyEvent.VK_Q);
+    public SimpleLogoView(ControllerSimpleLogo controller) {
+        super("un logo tout simple");
+        this.feuilleDessinView = new FeuilleDessinView();
+        this.controller = controller;
+        logoInit();
 
-		JMenu menuCommandes=new JMenu("Commandes"); // on installe le premier menu
-		menubar.add(menuCommandes);
-		addMenuItem(menuCommandes, "Avancer", "Avancer", -1);
-		addMenuItem(menuCommandes, "Droite", "Droite", -1);
-		addMenuItem(menuCommandes, "Gauche", "Gauche", -1);
-		addMenuItem(menuCommandes, "Lever Crayon", "Lever", -1);
-		addMenuItem(menuCommandes, "Baisser Crayon", "Baisser", -1);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent arg0) {
+                super.windowClosing(arg0);
+                System.exit(0);
+            }
+        });
+    }
 
-		JMenu menuHelp=new JMenu("Aide"); // on installe le premier menu
-		menubar.add(menuHelp);
-		addMenuItem(menuHelp, "Aide", "Help", -1);
-		addMenuItem(menuHelp, "A propos", "About", -1);
+    public void addTortue(Tortue tortue) {
+        feuilleDessinView.addTortue(tortue);
+    }
 
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public void logoInit() {
+        getContentPane().setLayout(new BorderLayout(10, 10));
 
-		// les boutons du bas
-		JPanel p2 = new JPanel(new GridLayout());
-		JButton b20 = new JButton("Proc1");
-		p2.add(b20);
-		b20.addActionListener(controller);
-		JButton b21 = new JButton("Proc2");
-		p2.add(b21);
-		b21.addActionListener(controller);
-		JButton b22 = new JButton("Proc3");
-		p2.add(b22);
-		b22.addActionListener(controller);
+        // Boutons
+        JToolBar toolBar = new JToolBar();
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(toolBar);
+
+        getContentPane().add(buttonPanel, "North");
+
+        addButton(toolBar, "Effacer", "Nouveau dessin", "/icons/index.png");
+
+        toolBar.add(Box.createRigidArea(HGAP));
+        inputValue = new JTextField("45", 5);
+        toolBar.add(inputValue);
+        addButton(toolBar, "Add", "Add", null);
+        addButton(toolBar, "Avancer", "Avancer 50", null);
+        addButton(toolBar, "Droite", "Droite 45", null);
+        addButton(toolBar, "Gauche", "Gauche 45", null);
+        addButton(toolBar, "Lever", "Lever Crayon", null);
+        addButton(toolBar, "Baisser", "Baisser Crayon", null);
+
+        String[] colorStrings = {"noir", "bleu", "cyan", "gris fonce", "rouge",
+                "vert", "gris clair", "magenta", "orange",
+                "gris", "rose", "jaune"};
+
+        // Create the combo box
+        toolBar.add(Box.createRigidArea(HGAP));
+        JLabel colorLabel = new JLabel("   Couleur: ");
+        toolBar.add(colorLabel);
+        JComboBox colorList = new JComboBox(colorStrings);
+        toolBar.add(colorList);
+
+        colorList.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox) e.getSource();
+                int n = cb.getSelectedIndex();
+            }
+        });
 
 
+        // Menus
+        JMenuBar menubar = new JMenuBar();
+        setJMenuBar(menubar);    // on installe le menu bar
+        JMenu menuFile = new JMenu("File"); // on installe le premier menu
+        menubar.add(menuFile);
 
-		getContentPane().add(p2,"South");
+        addMenuItem(menuFile, "Effacer", "Effacer", KeyEvent.VK_N);
+        addMenuItem(menuFile, "Quitter", "Quitter", KeyEvent.VK_Q);
 
-		getContentPane().add(feuilleDessinView,"Center");
-		
-		// Deplacement de la tortue au centre de la feuille;
+        JMenu menuCommandes = new JMenu("Commandes"); // on installe le premier menu
+        menubar.add(menuCommandes);
+        addMenuItem(menuCommandes, "Add tortue", "add", -1);
+        addMenuItem(menuCommandes, "Avancer", "Avancer", -1);
+        addMenuItem(menuCommandes, "Droite", "Droite", -1);
+        addMenuItem(menuCommandes, "Gauche", "Gauche", -1);
+        addMenuItem(menuCommandes, "Lever Crayon", "Lever", -1);
+        addMenuItem(menuCommandes, "Baisser Crayon", "Baisser", -1);
 
-		pack();
-		setVisible(true);
-	}
+        JMenu menuHelp = new JMenu("Aide"); // on installe le premier menu
+        menubar.add(menuHelp);
+        addMenuItem(menuHelp, "Aide", "Help", -1);
+        addMenuItem(menuHelp, "A propos", "About", -1);
 
-	public String getInputValue() {
-		String s = inputValue.getText();
-		return(s);
-	}
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // les boutons du bas
+        JPanel p2 = new JPanel(new GridLayout());
+        JButton b20 = new JButton("Proc1");
+        p2.add(b20);
+        b20.addActionListener(controller);
+        JButton b21 = new JButton("Proc2");
+        p2.add(b21);
+        b21.addActionListener(controller);
+        JButton b22 = new JButton("Proc3");
+        p2.add(b22);
+        b22.addActionListener(controller);
 
 
-	// efface tout et reinitialise la feuille
-	public void effacer() {
+        getContentPane().add(p2, "South");
+
+        getContentPane().add(feuilleDessinView, "Center");
+
+        // Deplacement de la tortue au centre de la feuille;
+
+        pack();
+        setVisible(true);
+    }
+
+    public String getInputValue() {
+        String s = inputValue.getText();
+        return (s);
+    }
+
+
+    // efface tout et reinitialise la feuille
+    public void effacer() {
 		/*
 		feuille.reset();
 		feuille.repaint();
@@ -163,43 +164,41 @@ public class SimpleLogoView extends JFrame {
 		// Replace la tortue au centre
 		Dimension size = feuille.getSize();
 		courante.setPosition(size.width/2, size.height/2);*/
-	}
+    }
 
-	//utilitaires pour installer des boutons et des menus
-	public void addButton(JComponent p, String name, String tooltiptext, String imageName) {
-		JButton b;
-		if ((imageName == null) || (imageName.equals(""))) {
-			b = (JButton)p.add(new JButton(name));
-		}
-		else {
-			java.net.URL u = this.getClass().getResource(imageName);
-			if (u != null) {
-				ImageIcon im = new ImageIcon (u);
-				b = (JButton) p.add(new JButton(im));
-			}
-			else
-				b = (JButton) p.add(new JButton(name));
-			b.setActionCommand(name);
-		}
+    //utilitaires pour installer des boutons et des menus
+    public void addButton(JComponent p, String name, String tooltiptext, String imageName) {
+        JButton b;
+        if ((imageName == null) || (imageName.equals(""))) {
+            b = (JButton) p.add(new JButton(name));
+        } else {
+            java.net.URL u = this.getClass().getResource(imageName);
+            if (u != null) {
+                ImageIcon im = new ImageIcon(u);
+                b = (JButton) p.add(new JButton(im));
+            } else
+                b = (JButton) p.add(new JButton(name));
+            b.setActionCommand(name);
+        }
 
-		b.setToolTipText(tooltiptext);
-		b.setBorder(BorderFactory.createRaisedBevelBorder());
-		b.setMargin(new Insets(0,0,0,0));
-		b.addActionListener(controller);
-	}
+        b.setToolTipText(tooltiptext);
+        b.setBorder(BorderFactory.createRaisedBevelBorder());
+        b.setMargin(new Insets(0, 0, 0, 0));
+        b.addActionListener(controller);
+    }
 
-	public void addMenuItem(JMenu m, String label, String command, int key) {
-		JMenuItem menuItem;
-		menuItem = new JMenuItem(label);
-		m.add(menuItem);
+    public void addMenuItem(JMenu m, String label, String command, int key) {
+        JMenuItem menuItem;
+        menuItem = new JMenuItem(label);
+        m.add(menuItem);
 
-		menuItem.setActionCommand(command);
-		menuItem.addActionListener(controller);
-		if (key > 0) {
-			if (key != KeyEvent.VK_DELETE)
-				menuItem.setAccelerator(KeyStroke.getKeyStroke(key, Event.CTRL_MASK, false));
-			else
-				menuItem.setAccelerator(KeyStroke.getKeyStroke(key, 0, false));
-		}
-	}
+        menuItem.setActionCommand(command);
+        menuItem.addActionListener(controller);
+        if (key > 0) {
+            if (key != KeyEvent.VK_DELETE)
+                menuItem.setAccelerator(KeyStroke.getKeyStroke(key, Event.CTRL_MASK, false));
+            else
+                menuItem.setAccelerator(KeyStroke.getKeyStroke(key, 0, false));
+        }
+    }
 }
